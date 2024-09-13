4d71feeebe83df1710420d088c8aaec253a1e5a4
Mostary Jahan <mostaryjahan01@gmail.com>
2024-09-13 18:36:21 +0600

    added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
 
 const Counter = () => {
-    const [count , setCount] = useState(0);
 
     const handleIncrement = () => {
-        setCount((count)=> count + 1);
     }
     const handleDecrement = () => {
-        setCount((count)=> count - 1);
     }
+
     return (
         <div>
             <h3>Counter App</h3>
-            <h4>Count: {count}</h4>
+            <h4>Count: 0</h4>
             <button onClick={handleIncrement}>Increment</button>
             <button onClick={handleDecrement}>Decrement</button>
         </div>
